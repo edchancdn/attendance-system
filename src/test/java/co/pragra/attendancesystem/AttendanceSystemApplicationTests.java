a8dd898f849168d69f665e01fe1606f3491aaee7
edchancdn <edchan.cdn@gmail.com>
2022-11-12 16:31:48 -0500

    initial commit

---
+++ src/test/java/co/pragra/attendancesystem/AttendanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package co.pragra.attendancesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
