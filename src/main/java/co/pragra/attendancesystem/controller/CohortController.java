d85bff4711f347ec63a3bdaa98a01ed37b3d8a27
edchancdn <edchan.cdn@gmail.com>
2022-11-22 11:29:39 -0500

    [feature/add-cohort-students-services]

--- src/main/java/co/pragra/attendancesystem/controller/CohortController.java
+++ src/main/java/co/pragra/attendancesystem/controller/CohortController.java
@@ -2,7 +2,10 @@ package co.pragra.attendancesystem.controller;
 
 import co.pragra.attendancesystem.dto.ErrorResponse;
 import co.pragra.attendancesystem.entity.Cohort;
+import co.pragra.attendancesystem.entity.Session;
+import co.pragra.attendancesystem.entity.Student;
 import co.pragra.attendancesystem.repo.CohortRepo;
+import co.pragra.attendancesystem.repo.StudentRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,11 +23,14 @@ import java.util.Optional;
 @RequestMapping("/api")
 public class CohortController {
 
-    @Autowired
+    //@Autowired
     private CohortRepo repo;
 
-    public CohortController(CohortRepo repo) {
+    private StudentRepo studentRepo;
+
+    public CohortController(CohortRepo repo, StudentRepo studentRepo) {
         this.repo = repo;
+        this.studentRepo = studentRepo;
     }
 
     @GetMapping("/cohort")
@@ -125,7 +131,15 @@ public class CohortController {
             try {
                 repo.deleteById(id);
                 if (!repo.findById(id).isPresent()) {
-                    return ResponseEntity.status(200).body(byId.get());
+                    Cohort rCohort = new Cohort();
+                    rCohort.setId(byId.get().getId());
+                    rCohort.setName(byId.get().getName());
+                    rCohort.setCourseName(byId.get().getCourseName());
+                    rCohort.setStartDate(byId.get().getStartDate());
+                    rCohort.setEndDate(byId.get().getEndDate());
+                    rCohort.setStudents(null);
+                    rCohort.setSessions(null);
+                    return ResponseEntity.status(200).body(rCohort);
                 } else {
                     ErrorResponse errorR = ErrorResponse.builder()
                             .appId("AMS-C6")
@@ -151,6 +165,126 @@ public class CohortController {
                     .errorCode(404)
                     .dateTime(new Date())
                     .message(String.format("Cohort with ID = [%s] not found in database", id))
+                    .build();
+            return ResponseEntity.status(404).body(errorR);
+        }
+    }
+
+    /*
+    Given a Cohort ID and a list of Student IDs,
+    Add the corresponding students into existing cohort.students
+    */
+    @PutMapping("/cohort/{cohortId}/student")
+    public ResponseEntity<?> addStudentToCohort(
+            @RequestBody List<Long> students,
+            @PathVariable Long cohortId) {
+        // get the session to be updated
+        Optional<Cohort> byId = repo.findById(cohortId);
+        if (byId.isPresent()) {
+            if (students.size() > 0) {
+                Cohort cohortToUpdate = byId.get();
+                // get the list to be updated
+                List<Student> listToUpdate = cohortToUpdate.getStudents();
+                for (Long sId: students) {
+                    // get the student to be added to the list
+                    Optional<Student> stById = studentRepo.findById(sId);
+                    if (stById.isPresent()) {
+                        Student studentAdd = stById.get();
+                        // check if student does not exist in the list
+                        if (!listToUpdate.contains(studentAdd)) {
+                            listToUpdate.add(studentAdd);
+                        }
+                    }
+                }
+                cohortToUpdate.setStudents(listToUpdate);
+                try {
+                    Cohort save = repo.save(cohortToUpdate);
+                    return ResponseEntity.status(200).body(save);
+                } catch (Exception e) {
+                    // LOG exception
+                    ErrorResponse errorR = ErrorResponse.builder()
+                            .appId("AMS-C7")
+                            .errorCode(500)
+                            .dateTime(new Date())
+                            .message(String.format("Student not added to cohort"))
+                            .build();
+                    return ResponseEntity.status(500).body(errorR);
+                }
+            } else {
+                ErrorResponse errorR = ErrorResponse.builder()
+                        .appId("AMS-C7")
+                        .errorCode(204)
+                        .dateTime(new Date())
+                        .message(String.format("Empty students list provided"))
+                        .build();
+                return ResponseEntity.status(204).body(errorR);
+            }
+        } else {
+            ErrorResponse errorR = ErrorResponse.builder()
+                    .appId("AMS-C7")
+                    .errorCode(404)
+                    .dateTime(new Date())
+                    .message(String.format("Cohort with ID = [%s] not found in database", cohortId))
+                    .build();
+            return ResponseEntity.status(404).body(errorR);
+        }
+    }
+
+    /*
+    Given a Cohort ID and a list of Student IDs
+    Remove the corresponding students from existing cohort.students
+     */
+    @DeleteMapping("/cohort/{cohortId}/student")
+    public ResponseEntity<?> deleteStudentFromCohort(
+            @RequestBody List<Long> students,
+            @PathVariable Long cohortId) {
+        // get the cohort to be updated
+        Optional<Cohort> byId = repo.findById(cohortId);
+        if (byId.isPresent()) {
+            if (students.size() > 0) {
+                Cohort cohortToUpdate = byId.get();
+                // get the list to be updated
+                List<Student> listToUpdate = cohortToUpdate.getStudents();
+                for (Long sId: students) {
+                    // get the student to be removed from the list
+                    Optional<Student> stById = studentRepo.findById(sId);
+                    if (stById.isPresent()) {
+                        Student studentRemove = stById.get();
+                        // check if student exist in the list
+                        if (listToUpdate.contains(studentRemove)) {
+                            listToUpdate.remove(studentRemove);
+                        }
+                    }
+                }
+                cohortToUpdate.setStudents(listToUpdate);
+                try {
+                    Cohort save = repo.save(cohortToUpdate);
+                    return ResponseEntity.status(200).body(save);
+                } catch (Exception e) {
+                    // LOG exception
+                    ErrorResponse errorR = ErrorResponse.builder()
+                            .appId("AMS-C8")
+                            .errorCode(500)
+                            .dateTime(new Date())
+                            .message(String.format("Student not removed from cohort"))
+                            .build();
+                    return ResponseEntity.status(500).body(errorR);
+                }
+            } else {
+                ErrorResponse errorR = ErrorResponse.builder()
+                        .appId("AMS-C8")
+                        .errorCode(204)
+                        .dateTime(new Date())
+                        .message(String.format("Empty students list provided"))
+                        .build();
+                return ResponseEntity.status(204).body(errorR);
+            }
+        } else {
+            ErrorResponse errorR = ErrorResponse.builder()
+                    .appId("AMS-C8")
+                    .errorCode(404)
+                    .dateTime(new Date())
+                    .message(String.format("Cohort with ID = [%s] not found in database", cohortId))
                     .build();
             return ResponseEntity.status(404).body(errorR);
         }
